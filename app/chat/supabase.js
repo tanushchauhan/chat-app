23cce3fa23e9971e869a840e9036b2d710e2e0d5
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-29 01:45:35 -0500

    Setted up supabase

---
+++ app/chat/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://bhzptjlfocqzryogekau.supabase.co";
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoenB0amxmb2NxenJ5b2dla2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5MTM0NDQsImV4cCI6MjA0MDQ4OTQ0NH0.21dMFQA55YS2qGVbFt6G6cjEsHFGqZiZGx9zjzA0PW0`;
+export const supabase = createClient(supabaseUrl, supabaseKey);
