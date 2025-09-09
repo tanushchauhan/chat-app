7291f5423f9257a43e87977021ec17cd82f7eda9
Tanush Chauhan <tanushchauhan07@gmail.com>
2025-09-09 16:58:59 -0500

    Change Supabase instance in realtime components

--- app/chat/supabase.js
+++ app/chat/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bhzptjlfocqzryogekau.supabase.co";
+const supabaseUrl = "https://kcnzfmmjfvxdjtqvpghe.supabase.co";
 
 // Row level security is enabled. Only read and insert access using this key.
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoenB0amxmb2NxenJ5b2dla2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5MTM0NDQsImV4cCI6MjA0MDQ4OTQ0NH0.21dMFQA55YS2qGVbFt6G6cjEsHFGqZiZGx9zjzA0PW0`;
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjbnpmbW1qZnZ4ZGp0cXZwZ2hlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NTI3NjgsImV4cCI6MjA3MzAyODc2OH0.ZJcc32LxJPFHRriJxe5kwr-mM2VwH8AE7Y8f8zvyog4`;
 export const supabase = createClient(supabaseUrl, supabaseKey);
